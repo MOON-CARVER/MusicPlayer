c376d4241a0f84271be686727dac3d369d8ec74c
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-02-07 01:52:05 +0530

    Basic music player
    Created a basic music player ui to check storage for songs

--- vs_code/lib/main.dart
+++ vs_code/lib/main.dart
@@ -1,20 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Melophile',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const AllSongs(),
+    );
+  }
+}
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class AllSongs extends StatefulWidget {
+  const AllSongs({Key? key}) : super(key: key);
+  @override
+  _AllSongsState createState() => _AllSongsState();
+}
+
+class _AllSongsState extends State<AllSongs> {
+  final _audioQuery = new OnAudioQuery();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Music Player"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+          sortType: null,
+          orderType: OrderType.ASC_OR_SMALLER,
+          uriType: UriType.EXTERNAL,
+          ignoreCase: true 
+        ),
+        builder: (context, item){
+          if(item.data == null){
+            return Center(child: CircularProgressIndicator());
+          }
+          if(item.data!.isEmpty) {
+            return Center(child: Text("No Songs Found"));
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              leading: const Icon(Icons.music_note),
+              title: Text(item.data![index].displayNameWOExt),
+              subtitle: const Text("Artist Name\t Duration"),
+              trailing: const Icon(Icons.more_horiz),
+            ),
+            itemCount: item.data!.length,
+          );
+        },
+      ),
+    );
+  }
 }
