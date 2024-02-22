3e8623c8c9012ff3c32cc61bf7d4190d1970fd01
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-02-23 04:42:29 +0530

    Update bottom_nav_bar.dart

--- flutter_application_1/lib/bottom_nav_bar.dart
+++ flutter_application_1/lib/bottom_nav_bar.dart
@@ -2,29 +2,33 @@
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   final String songTitle;
-  final VoidCallback onPlayPause; // Add a callback function
-  final bool isPlaying; 
+  final VoidCallback onPlayPause;
 
+  const BottomNavBar({
+    Key? key,
+    required this.songTitle,
+    required this.onPlayPause,
+  }) : super(key: key);
 
-  const BottomNavBar(
-      {Key? key, 
-      required this.songTitle, 
-      required this.onPlayPause,
-      required this.isPlaying})
-      : super(key: key);
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  bool _isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 70,
-      color:Color.fromARGB(255, 17, 26, 36),
+      color: Color.fromARGB(255, 17, 26, 36),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
         child: Container(
           height: 60.0,
-          width:100 ,
+          width: 100,
           decoration: BoxDecoration(
             color: Color.fromARGB(255, 14, 42, 57),
             borderRadius: BorderRadius.circular(30.0),
@@ -34,24 +38,27 @@ class BottomNavBar extends StatelessWidget {
             children: [
               Expanded(
                 child: Marquee(
-                  text: ' $songTitle',
-
+                  text: ' ${widget.songTitle}',
                   style: const TextStyle(color: Colors.white),
-
-                  blankSpace:20.0,
-                  velocity: 50.0 ,
-                  pauseAfterRound:Duration(seconds:3),
-                  startPadding:10,
-
-
+                  blankSpace: 20.0,
+                  velocity: 50.0,
+                  pauseAfterRound: Duration(seconds: 3),
+                  startPadding: 10,
                 ),
               ),
               IconButton(
-                // Add IconButton
-                icon:  Icon(
-                  isPlaying ?Icons.pause_circle_rounded:Icons.play_arrow_rounded,
-                  color: Colors.amberAccent,),
-                onPressed: onPlayPause,
+                icon: Icon(
+                  _isPlaying
+                      ? Icons.pause_circle_rounded
+                      : Icons.play_arrow_rounded,
+                  color: Colors.amberAccent,
+                ),
+                onPressed: () {
+                  widget.onPlayPause();
+                  setState(() {
+                    _isPlaying = !_isPlaying;
+                  });
+                },
               ),
             ],
           ),
