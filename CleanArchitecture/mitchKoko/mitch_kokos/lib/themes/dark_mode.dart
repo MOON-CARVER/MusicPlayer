7ae78b23399238a66a2e3469408d42437cab9cbc
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-02-26 01:51:44 +0530

    mitchKokos music player UI

---
+++ CleanArchitecture/mitchKoko/mitch_kokos/lib/themes/dark_mode.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+        background: Color.fromARGB(255, 180, 45, 198),
+        primary: Colors.grey.shade500,
+        secondary: Colors.grey.shade200,
+        inversePrimary: Colors.grey.shade900));
---
+++ CleanArchitecture/mitchKoko/mitch_kokos/lib/themes/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mitch_kokos/themes/dark_mode.dart';
+import 'package:mitch_kokos/themes/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get ThemeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    }else{
+      themeData = lightMode
+    }
+  }
+}
