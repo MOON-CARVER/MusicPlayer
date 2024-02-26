b4629503aa8146e0fbe78bee25c123a94e810187
sauravdani <sauravdani7@gmail.com>
2024-02-26 11:40:54 +0530

    Update playScreen.dart

--- MainApp/lib/Screens/playScreen.dart
+++ MainApp/lib/Screens/playScreen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class playScreen extends StatelessWidget {
+// class playScreen extends StatelessWidget {
+//   const playScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ElevatedButton(child: Icon(Icons.chevron_left), onPressed: () {}),
+//     );
+//   }
+// }
+
+class playScreen extends StatefulWidget {
   const playScreen({super.key});
 
   @override
+  State<playScreen> createState() => _playScreenState();
+}
+
+class _playScreenState extends State<playScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: TextButton(child: Icon(Icons.arrow_drop_down), onPressed: () {}),
+    );
   }
 }
