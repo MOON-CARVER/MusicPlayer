5b5705e58c1fc13740456782dc3489237d4a9395
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-02-28 22:12:07 +0530

    let's start again

--- UiBuild/lib/core/data/repositories/i_songs_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:clean_build/core/data/models/song.dart';
-
-abstract class ISongsRepository {
-  Future<List<Song>> getAllSongs();
-  // More methods for filtering, searching, etc.
-}
---
+++ firebase_getx/README.md
@@ -0,0 +1,3 @@
+# firebase_getx
+
+A new Flutter project.
