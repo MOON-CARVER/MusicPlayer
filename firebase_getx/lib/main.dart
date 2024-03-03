98afcabf3ec5839eee157a9db349875c228c99db
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-03-03 17:46:57 +0530

    start again!

---
+++ firebase_getx/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_getx/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'M E L O P H I L E',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        )
+      ),
+      home: const HomePage(),
+      getPages: [
+        GetPage(name: '/', page:()=> const HomePage()),
+        //add more routes here
+        ],
+    );
+  }
+}
