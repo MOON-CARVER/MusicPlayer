107318d7128ee7e7c3ec4b2e90079f513ac08f72
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-03-03 18:15:49 +0530

    updated Ui , issue fixed

--- firebase_getx/lib/pages/home_page.dart
+++ firebase_getx/lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_getx/widgets/bottom_nav_bar.dart';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,47 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            const Color.fromARGB(255, 82, 35, 225).withOpacity(0.8),
+            const Color.fromARGB(255, 147, 119, 199).withOpacity(0.8),
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: const _CustomAppBar(),
+        bottomNavigationBar: const GoogleBottomBar(),
+        body: Container(),
+      ),
+    );
   }
+}
+
+class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: const CircleAvatar(
+            backgroundColor: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
 }
