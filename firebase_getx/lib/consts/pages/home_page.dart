e1222864818297eeb94782851c270da56b028371
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-03-03 11:37:52 +0530

    update scaffold

--- firebase_getx/lib/consts/pages/home_page.dart
+++ firebase_getx/lib/consts/pages/home_page.dart
@@ -6,39 +6,55 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(),
-      child: Scaffold(
-          appBar: _CustomAppBar(),
-          bottomNavigationBar: GoogleBottomBar(),
-          body: Column(
-            children: [
-              Text(
-                'Hello, World!',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          )),
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.deepPurple.shade800.withOpacity(0.8),
+            Colors.deepPurple.shade800.withOpacity(0.8)
+          ],
+        )),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: Text('M E L O P H I L E'),
+              leading: const Icon(Icons.menu_book_rounded),
+            ),
+            bottomNavigationBar: GoogleBottomBar(),
+            body: Column(
+              children: [
+                Text(
+                  'Hello, World!',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
 
-class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const _CustomAppBar({
-    super.key,
-  });
+// class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   const _CustomAppBar({
+//     super.key,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text(
-        'M E L O P H I L E',
-      ), //app bar properties ,and icons
-      elevation: 0,
-      leading: const Icon(Icons.grid_view_rounded),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       backgroundColor: Colors.purple,
+//       title: Text(
+//         'M E L O P H I L E',
+//       ),
+//       //app bar properties ,and icons
+//       // elevation: 0,
+//       leading: const Icon(Icons.grid_view_rounded),
+//     );
+//   }
 
-  @override
-  Size get preferredSize => const Size.fromHeight(56.0);
-}
+//   @override
+//   Size get preferredSize => const Size.fromHeight(56.0);
+// }
--- firebase_getx/lib/main.dart
+++ firebase_getx/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_getx/consts/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,27 +12,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ThemeMode themeMode = ThemeMode.system;
-    // Opt in/out on Material 3
+    
     bool useMaterial3 = true;
 
-    const FlexScheme usedScheme = FlexScheme.mandyRed;
+    
 
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: ('M E L O P H I L E '),
-      theme: FlexThemeData.light(
-        scheme: usedScheme,
-        appBarElevation: 0.5,
-        useMaterial3: useMaterial3,
+      theme: ThemeData(
+        textTheme: 
+        Theme.of(context).textTheme.apply(
+        bodyColor: Colors.white,
+        displayColor: Colors.white,)
       ),
-
-      darkTheme: FlexThemeData.dark(
-        scheme: usedScheme,
-        appBarElevation: 2,
-        useMaterial3: useMaterial3,
-      ),
-// theme data here
+    
 
 //
       home: HomePage(),
