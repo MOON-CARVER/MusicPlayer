ef6583dbd7fd1c4edcdb73a5bc6f2224fc0e7af6
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2024-03-15 16:07:32 +0530

    reBuilding flutter_application_1

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,91 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:on_audio_query/on_audio_query.dart';
-import 'audio_backend.dart';
-import 'bottom_nav_bar.dart';
+import 'package:flutter_application_1/views/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Songs(),
+    home: HomePage(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class Songs extends StatefulWidget {
-  const Songs({Key? key}) : super(key: key);
-
-  @override
-  _SongsState createState() => _SongsState();
-}
-
-class _SongsState extends State<Songs> {
-  final AudioBackend _audioBackend = AudioBackend();
-  String? _selectedSongTitle;
-  bool _isPlaying = false;
-
-  void _handlePlayPause() {
-    if (_isPlaying) {
-      _audioBackend.pause();
-    } else {
-      _audioBackend.resume();
-    }
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
-  }
-
-  @override
-  void dispose() {
-    _audioBackend.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _audioBackend.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Music Player'),
-      ),
-      body: FutureBuilder<List<SongModel>>(
-        future: _audioBackend.querySongs(),
-        builder:
-            (BuildContext context, AsyncSnapshot<List<SongModel>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data?.length ?? 0,
-              itemBuilder: (BuildContext context, int index) {
-                SongModel song = snapshot.data![index];
-                return ListTile(
-                  leading: const Icon(Icons.music_note),
-                  title: Text(song.title),
-                  onTap: () {
-                    _audioBackend.playSong(song.data);
-                    setState(() {
-                      _selectedSongTitle = song.title;
-                    });
-                  },
-                );
-              },
-            );
-          }
-        },
-      ),
-      bottomNavigationBar: _selectedSongTitle !=
-              null // bottom sheet showing playing song with buttons for play pause
-          ? BottomNavBar(
-              songTitle: _selectedSongTitle!,
-              onPlayPause: _handlePlayPause,
-            )
-          : null,
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    title: 'M E L O P H I L E',
+    theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+  );
 }
